da9b9ca3df87eb0c442e45a1568f8fafcb4b098e
CarlosArauco8556 <129557200+CarlosArauco8556@users.noreply.github.com>
2025-09-27 00:38:31 -0300

    feat: add and config ocelot

--- parla-metro-api-main/Program.cs
+++ parla-metro-api-main/Program.cs
@@ -1,4 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -13,5 +23,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+await app.UseOcelot();
 
 app.Run();
